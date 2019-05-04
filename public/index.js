872bb8b3b48d4900d69fd82d7f0504bfee6d68f9
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-05-04 16:50:32 +0200

    wip

--- public/index.js
+++ public/index.js
@@ -580,8 +580,8 @@ async function submitSearch() {
         }
         let searchDate = el('#search-date').value || null;
         if (searchDate) {
-            searchSpec.date = new Date(searchDate).getTime();
-            searchSpec.dateInterval = ((1 * el('#search-date-day-interval').value) || 0) * 1000 * 60 * 60 * 24;
+            searchSpec.date = new Date(searchDate).getTime() + 1000 * 60 * 60 * 12;
+            searchSpec.dateInterval = (parseInt(el('#search-date-day-interval').value || '0')) * 1000 * 60 * 60 * 12;
         }
         const headers = new Headers();
         headers.set('Content-Type', 'application/json');
--- src/index.ts
+++ src/index.ts
@@ -759,8 +759,8 @@ async function submitSearch() {
 
         let searchDate = el<HTMLInputElement>('#search-date').value || null
         if (searchDate) {
-            searchSpec.date = new Date(searchDate).getTime()
-            searchSpec.dateInterval = ((1 * (el<HTMLInputElement>('#search-date-day-interval').value as any)) || 0) * 1000 * 60 * 60 * 24
+            searchSpec.date = new Date(searchDate).getTime() + 1000 * 60 * 60 * 12
+            searchSpec.dateInterval = (parseInt(el<HTMLInputElement>('#search-date-day-interval').value || '0')) * 1000 * 60 * 60 * 12
         }
 
         const headers = new Headers()
