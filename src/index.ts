709304d0a3c21db27aeecec793ecc8826c483e5d
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-07-17 19:50:55 +0200

    ui

--- src/index.ts
+++ src/index.ts
@@ -350,12 +350,34 @@ async function restartFilePool() {
         if (file.mimeType != 'application/octet-stream')
             mimeTypes.push(file.mimeType)
 
-        let links = mimeTypes.map((mimeType, index) => `[<a href='${HEXA_BACKUP_BASE_URL}/sha/${file.sha}/content?type=${mimeType}${index == 0 ? `&fileName=${file.fileName}` : ''}' >${EXTENDED ? mimeType : (index == 0 ? 'dl' : (mimeType.indexOf('/') ? mimeType.substr(mimeType.indexOf('/') + 1) : mimeType))}</a>]`).join(' ')
+        const contentUrl = (mimeType: string, index) => {
+            return `${HEXA_BACKUP_BASE_URL}/sha/${file.sha}/content?type=${mimeType}${index == 0 ? `&fileName=${file.fileName}` : ''}`
+        }
+
+        const contentLink = (mimeType: string, index) => {
+            const displayedMimeType = (() => {
+                if (EXTENDED) {
+                    return mimeType
+                }
+                else if (index == 0) {
+                    return 'dl'
+                }
+                else {
+                    return mimeType.indexOf('/') < 0 ? mimeType : mimeType.substr(mimeType.indexOf('/') + 1)
+                }
+            })()
+
+            return `[<a href='${contentUrl(mimeType, index)}'>${displayedMimeType}</a>]`
+        }
+
+        let links = mimeTypes
+            .map(contentLink)
+            .join(' ')
 
         let htmlPrefix = ''
         let html = ''
         let classes = []
-        let action = 'false'
+        let action: string = null
 
         if (file.mimeType.startsWith('audio/')) {
             classes.push(`audio-${audioIndex}`)
@@ -381,12 +403,19 @@ async function restartFilePool() {
         }
         else {
             let displayedName: string = file.fileName.substr(currentPrefix.length)
-            if (action != 'false') {
+            let actionJs = ''
+            if (action) {
                 let ie = displayedName.lastIndexOf('.')
                 if (ie)
                     displayedName = displayedName.substr(0, ie)
+
+                actionJs = `href='#' onclick='event.preventDefault() || ${action}'`
             }
-            html = `<a href='#' onclick='event.preventDefault() || ${action}'>${displayedName}</a> <span class='small'>${htmlParents} ${likeHtml}</span>`
+            else {
+                actionJs = `href='${contentUrl(mimeTypes[0], 0)}'`
+            }
+
+            html = `<a ${actionJs}>${displayedName}</a> <span class='small'>${htmlParents} ${likeHtml}</span>`
 
             if (displayedSortOrder == 'date') {
                 let date = `<span class='small'>${displayDate(file.lastWrite)}</span>`
