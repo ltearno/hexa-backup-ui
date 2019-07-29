c3d3ae4a3009b5edadb3c1a9193befe215d9551c
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-07-30 00:32:27 +0200

    wip

--- public/templates.js
+++ public/templates.js
@@ -1,16 +1,7 @@
-﻿(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./ui-tool"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    const UiTools = require("./ui-tool");
-    const templateHtml = `
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const UiTools = require("./ui-tool");
+const templateHtml = `
 <div class='mui-container-fluid'>
     <div class="mui--text-center">
         <h1>Raccoon</h1>
@@ -25,13 +16,12 @@
         <br /><a href="#">Browse</a> - <a href="#">Settings</a>
     </div>
 </div>`;
-    exports.searchPanel = {
-        create: () => {
-            return templateHtml;
-        },
-        createElement: () => {
-            return UiTools.elFromHtml(exports.searchPanel.create());
-        }
-    };
-});
+exports.searchPanel = {
+    create: () => {
+        return templateHtml;
+    },
+    createElement: () => {
+        return UiTools.elFromHtml(exports.searchPanel.create());
+    }
+};
 //# sourceMappingURL=templates.js.map
--- public/ui-tool.js
+++ public/ui-tool.js
@@ -1,27 +1,17 @@
-﻿(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    function el(id) {
-        return document.getElementById(id);
-    }
-    exports.el = el;
-    function els(element, selector) {
-        return element.querySelectorAll(selector);
-    }
-    exports.els = els;
-    function elFromHtml(html) {
-        const parent = document.createElement('div');
-        parent.innerHTML = html;
-        return parent.children.item(0);
-    }
-    exports.elFromHtml = elFromHtml;
-});
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function el(id) {
+    return document.getElementById(id);
+}
+exports.el = el;
+function els(element, selector) {
+    return element.querySelectorAll(selector);
+}
+exports.els = els;
+function elFromHtml(html) {
+    const parent = document.createElement('div');
+    parent.innerHTML = html;
+    return parent.children.item(0);
+}
+exports.elFromHtml = elFromHtml;
 //# sourceMappingURL=ui-tool.js.map
