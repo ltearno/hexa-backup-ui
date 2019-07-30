07b88e36f79cab7e23304b51e41bc9003ac6825e
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-07-30 13:25:56 +0200

    wip

--- public/auth.js
+++ public/auth.js
@@ -5,21 +5,28 @@ function wait(duration) {
     return new Promise(resolve => setTimeout(resolve, duration));
 }
 class Auth {
+    onError() {
+        window.location.reload();
+    }
     async loop() {
         while (true) {
             try {
                 let response = await Network.postData(`https://home.lteconsulting.fr/auth`);
                 if (response && response.token) {
                     let res = await Network.getData(`https://home.lteconsulting.fr/well-known/v1/setCookie`, { 'Authorization': `Bearer ${response.token}` });
-                    if (!res || !res.lifetime)
+                    if (!res || !res.lifetime) {
                         console.error(`cannot setCookie`, res);
+                        this.onError();
+                    }
                 }
                 else {
                     console.error(`cannot obtain auth token`);
+                    this.onError();
                 }
             }
             catch (err) {
                 console.error(`cannot refresh auth (${err})`);
+                this.onError();
             }
             await wait(10000);
         }
--- src/auth.ts
+++ src/auth.ts
@@ -5,21 +5,29 @@ function wait(duration) {
 }
 
 class Auth {
+    private onError() {
+        window.location.reload()
+    }
+
     async loop() {
         while (true) {
             try {
                 let response: { token: string } = await Network.postData(`https://home.lteconsulting.fr/auth`)
                 if (response && response.token) {
                     let res: any = await Network.getData(`https://home.lteconsulting.fr/well-known/v1/setCookie`, { 'Authorization': `Bearer ${response.token}` })
-                    if (!res || !res.lifetime)
+                    if (!res || !res.lifetime) {
                         console.error(`cannot setCookie`, res)
+                        this.onError()
+                    }
                 }
                 else {
                     console.error(`cannot obtain auth token`)
+                    this.onError()
                 }
             }
             catch (err) {
                 console.error(`cannot refresh auth (${err})`)
+                this.onError()
             }
 
             await wait(10000)
