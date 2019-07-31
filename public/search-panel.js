f69618076c90a0e3f431bcacd30043fcb5a85324
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-07-31 21:37:07 +0200

    wip

--- public/search-panel.js
+++ public/search-panel.js
@@ -1,19 +1,16 @@
 ﻿"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const templates_1 = require("./templates");
-const TID_Title = 'title';
-const TID_SearchForm = 'form';
-const TID_SearchTerm = 'term';
 const templateHtml = `
 <div class='mui-container-fluid'>
     <div class="mui--text-center">
-        <h1 x-id="${TID_Title}" class="animated--quick">Raccoon</h1>
-        <form x-id="${TID_SearchForm}" class="mui-form--inline">
+        <h1 x-id="title" class="animated--quick">Raccoon</h1>
+        <h4 x-id="subTitle"></h4>
+        <form x-id="form" class="mui-form--inline">
             <!--this is a little hack to have things centered-->
             <div class="mui-btn mui-btn--flat" style="visibility: hidden;">🔍</div>
-            <div class="mui-textfield mui-textfield--float-label">
-                <input x-id="${TID_SearchTerm}" type="text" style="text-align: center;" autofocus>
-                <label>Search an audio title</label>
+            <div class="mui-textfield">
+                <input x-id="term" type="text" style="text-align: center;" autofocus>
             </div>
             <button role="submit" class="mui-btn mui-btn--flat">🔍</button>
         </form>
@@ -23,10 +20,14 @@ const templateHtml = `
 exports.searchPanel = {
     create: () => templates_1.createTemplateInstance(templateHtml),
     displayTitle: (template, displayed) => {
-        if (displayed)
+        if (displayed) {
             template.title.classList.remove('hexa--reduced');
-        else
+            template.subTitle.style.display = undefined;
+        }
+        else {
             template.title.classList.add('hexa--reduced');
+            template.subTitle.style.display = 'none';
+        }
     }
 };
 //# sourceMappingURL=search-panel.js.map
--- src/search-panel.ts
+++ src/search-panel.ts
@@ -2,24 +2,21 @@ import { TemplateElements, createTemplateInstance } from './templates'
 
 export interface SearchPanelElements extends TemplateElements {
     title: HTMLElement
+    subTitle: HTMLElement
     form: HTMLFormElement
     term: HTMLInputElement
 }
 
-const TID_Title = 'title'
-const TID_SearchForm = 'form'
-const TID_SearchTerm = 'term'
-
 const templateHtml = `
 <div class='mui-container-fluid'>
     <div class="mui--text-center">
-        <h1 x-id="${TID_Title}" class="animated--quick">Raccoon</h1>
-        <form x-id="${TID_SearchForm}" class="mui-form--inline">
+        <h1 x-id="title" class="animated--quick">Raccoon</h1>
+        <h4 x-id="subTitle"></h4>
+        <form x-id="form" class="mui-form--inline">
             <!--this is a little hack to have things centered-->
             <div class="mui-btn mui-btn--flat" style="visibility: hidden;">🔍</div>
-            <div class="mui-textfield mui-textfield--float-label">
-                <input x-id="${TID_SearchTerm}" type="text" style="text-align: center;" autofocus>
-                <label>Search an audio title</label>
+            <div class="mui-textfield">
+                <input x-id="term" type="text" style="text-align: center;" autofocus>
             </div>
             <button role="submit" class="mui-btn mui-btn--flat">🔍</button>
         </form>
@@ -31,9 +28,13 @@ export const searchPanel = {
     create: () => createTemplateInstance(templateHtml) as SearchPanelElements,
 
     displayTitle: (template: SearchPanelElements, displayed: boolean) => {
-        if (displayed)
+        if (displayed) {
             template.title.classList.remove('hexa--reduced')
-        else
+            template.subTitle.style.display = undefined
+        }
+        else {
             template.title.classList.add('hexa--reduced')
+            template.subTitle.style.display = 'none'
+        }
     }
 }
