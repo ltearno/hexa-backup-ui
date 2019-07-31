31f9e39f4883413d2118afb78096e17d1c36e024
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-07-31 13:24:29 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ searchPanel.form.addEventListener('submit', async event => {
             file.name = file.name.substring(0, dot)
         file.name = file.name.replace(/'_'/g, ' ')
         file.name = file.name.replace(/'  '/g, ' ')
-        file.name = file.name.replace(/'[ ]*[-][ ]*'/g, ' - ')
+        file.name = file.name.replace(/'7-y'/g, ' - ')
         return file
     })
 
