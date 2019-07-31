3802ff8199a93fd69e8e4e27236b7040333c8f7e
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-07-31 12:40:46 +0200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     mode: 'development',
     entry: './public/index.js',
-    devtool: 'source-map',
+    devtool: 'inline-source-map',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
