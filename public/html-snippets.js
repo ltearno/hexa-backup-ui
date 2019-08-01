402d2b5d2532b85955fc4e55a6752f4f8d30a6e8
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-08-01 12:53:09 +0200

    wip

---
+++ public/html-snippets.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Rest = require("./rest");
+function itemToHtml(f) {
+    if (f.mimeType == 'application/directory')
+        return `<div class="onclick"><i>${f.name} ...</i></div>`;
+    else if (f.mimeType == 'application/reference')
+        return `<div class="onclick"><i>${f.name} ...</i></div>`;
+    else if (f.mimeType.startsWith('audio/'))
+        return `<div x-for-sha="${f.sha && f.sha.substr(0, 5)}" class="onclick">${f.name}</div>`;
+    else
+        return `<div x-for-sha="${f.sha && f.sha.substr(0, 5)}" class="onclick"><a href="${Rest.getShaContentUrl(f.sha, f.mimeType, f.name, false)}" target="_blank">${f.name}</a> <a class="mui--text-dark-secondary" href="${Rest.getShaContentUrl(f.sha, f.mimeType, f.name, true)}">dl</a></div>`;
+}
+exports.itemToHtml = itemToHtml;
+//# sourceMappingURL=html-snippets.js.map
---
+++ src/html-snippets.ts
@@ -0,0 +1,12 @@
+import * as Rest from './rest'
+
+export function itemToHtml(f: Rest.FileDescriptor) {
+    if (f.mimeType == 'application/directory')
+        return `<div class="onclick"><i>${f.name} ...</i></div>`
+    else if (f.mimeType == 'application/reference')
+        return `<div class="onclick"><i>${f.name} ...</i></div>`
+    else if (f.mimeType.startsWith('audio/'))
+        return `<div x-for-sha="${f.sha && f.sha.substr(0, 5)}" class="onclick">${f.name}</div>`
+    else
+        return `<div x-for-sha="${f.sha && f.sha.substr(0, 5)}" class="onclick"><a href="${Rest.getShaContentUrl(f.sha, f.mimeType, f.name, false)}" target="_blank">${f.name}</a> <a class="mui--text-dark-secondary" href="${Rest.getShaContentUrl(f.sha, f.mimeType, f.name, true)}">dl</a></div>`
+}
