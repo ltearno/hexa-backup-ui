116214b40674eef4190fd3cee583f407b689413e
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-08-02 18:55:54 +0200

    yop

---
+++ public/image-detail.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Rest = require("./rest");
+const templates_1 = require("./templates");
+const template = `
+    <div class="x-image-detail">
+        <img x-id="image"/>
+        <div x-id="toolbar">
+        <button class="mui-btn mui-btn--flat mui-btn--flat">Previous</button>
+        <button class="mui-btn mui-btn--flat">Close</button>
+        <button class="mui-btn mui-btn--flat mui-btn--raised">Next</button>
+        </div>
+    </div>`;
+const element = templates_1.createTemplateInstance(template);
+function show(item, unroller) {
+    document.body.querySelector('header').style.display = 'none';
+    if (!element.root.isConnected)
+        document.body.appendChild(element.root);
+    element.image.src = Rest.getShaImageMediumThumbnailUrl(item.sha, item.mimeType);
+    element.image.alt = item.name;
+}
+exports.show = show;
+//# sourceMappingURL=image-detail.js.map
---
+++ src/image-detail.ts
@@ -0,0 +1,34 @@
+import * as Rest from './rest'
+import { TemplateElements, createTemplateInstance } from './templates'
+import * as Snippets from './html-snippets'
+
+const template = `
+    <div class="x-image-detail">
+        <img x-id="image"/>
+        <div x-id="toolbar">
+        <button class="mui-btn mui-btn--flat mui-btn--flat">Previous</button>
+        <button class="mui-btn mui-btn--flat">Close</button>
+        <button class="mui-btn mui-btn--flat mui-btn--raised">Next</button>
+        </div>
+    </div>`
+
+const element: {
+    root: HTMLElement
+    image: HTMLImageElement
+    toolbar: HTMLElement
+} = createTemplateInstance(template)
+
+export interface Unroller {
+    previous(): Rest.FileDescriptor
+    next(): Rest.FileDescriptor
+}
+
+export function show(item: Rest.FileDescriptor, unroller: Unroller) {
+    document.body.querySelector('header').style.display = 'none'
+
+    if (!element.root.isConnected)
+        document.body.appendChild(element.root)
+
+    element.image.src = Rest.getShaImageMediumThumbnailUrl(item.sha, item.mimeType)
+    element.image.alt = item.name
+}
