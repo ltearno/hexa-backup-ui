d3feae769a145eebff92240b9be8f5ac78d4e7cc
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-08-02 02:08:32 +0200

    wip

--- public/slideshow.js
+++ public/slideshow.js
@@ -11,7 +11,7 @@ const templateHtml = `
         speed: <input x-id="speed" type="range" min="100" max="3000" value="200"/>
         nb images: <input x-id="nbImages" type="range" min="1" max="50" value="12"/>
         interval: <input x-id="interval" type="range" min="0" max="100" value="50"/>
-        date: <input x-id="date" type="range" min="-${1000 * 60 * 60 * 24 * 365 * 20} max="0" value="0"/>
+        date: <input x-id="date" type="range" min="-${1000 * 60 * 60 * 24 * 365 * 20}" max="0" value="0"/>
         <div x-id="remark"></div>
     </div>
 </div>`;
