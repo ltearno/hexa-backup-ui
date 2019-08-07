6aeb36f87911cf7d64cdbf89d57b237a49d90d52
Arnaud Tournier <arnaud.tournier@stack-labs.com>
2019-08-07 12:58:00 +0200

    wip

--- src/html-snippets.ts
+++ src/html-snippets.ts
@@ -3,11 +3,11 @@ import * as Locations from './locations'
 
 export function itemToHtml(f: Rest.FileDescriptor, currentName: string) {
     if (f.mimeType == 'application/directory')
-        return `<div class="onclick"><i><a href='${Locations.getDirectoryUrl(f.sha, f.name, currentName)}'>${f.name}</a> ...</i></div>`
+        return `<div class="onclick"><i><a class='x-no-color' href='${Locations.getDirectoryUrl(f.sha, f.name, currentName)}'>${f.name}</a> ...</i></div>`
     else if (f.mimeType == 'application/reference')
-        return `<div class="onclick"><i><a href='${Locations.getReferenceUrl(f.name)}'>${f.name}</a> ...</i></div>`
+        return `<div class="onclick"><i><a class='x-no-color' href='${Locations.getReferenceUrl(f.name)}'>${f.name}</a> ...</i></div>`
     else if (f.mimeType == 'application/playlist')
-        return `<div class="onclick"><i><a href='${Locations.getPlaylistUrl(f.name)}'>${f.name}</a> ...</i></div>`
+        return `<div class="onclick"><i><a class='x-no-color' href='${Locations.getPlaylistUrl(f.name)}'>${f.name}</a> ...</i></div>`
     else if (f.mimeType.startsWith('audio/'))
         return `<div x-for-sha="${f.sha && f.sha.substr(0, 5)}" class="onclick">${f.name}</div>`
     else
